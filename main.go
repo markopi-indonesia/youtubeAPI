f44894695e6a93c2198fa0b03f31f805eb185878
Joel Marpaung <myitlife.jm@gmail.com>
2019-08-21 17:26:29 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"google.golang.org/api/youtube/v3"
+)
+
+var (
+	filename    = flag.String("filename", "", "Name of video file to upload")
+	title       = flag.String("title", "Test Title", "Video title")
+	description = flag.String("description", "Test Description", "Video description")
+	category    = flag.String("category", "22", "Video category")
+	keywords    = flag.String("keywords", "", "Comma separated list of video keywords")
+	privacy     = flag.String("privacy", "public", "Video privacy status")
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	// r.ParseMultipartForm(10 << 20)
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	// if *filename == "" {
+	// 	log.Fatalf("You must provide a filename of a video file to upload")
+	// }
+
+	client := getClient(youtube.YoutubeUploadScope)
+
+	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatalf("Error creating YouTube client: %v", err)
+	}
+
+	upload := &youtube.Video{
+		Snippet: &youtube.VideoSnippet{
+			Title:       *title,
+			Description: *description,
+			CategoryId:  *category,
+		},
+		Status: &youtube.VideoStatus{PrivacyStatus: *privacy},
+	}
+
+	// The API returns a 400 Bad Request response if tags is an empty string.
+	if strings.Trim(*keywords, "") != "" {
+		upload.Snippet.Tags = strings.Split(*keywords, ",")
+	}
+
+	call := service.Videos.Insert("snippet,status", upload)
+
+	// file, err := os.Open(*filename)
+	defer file.Close()
+	if err != nil {
+		log.Fatalf("Error opening : %v", err)
+	}
+
+	response, err := call.Media(file).Do()
+	handleError(err, "")
+	fmt.Printf("Upload successful! Video ID: %v\n", response.Id)
+
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	fmt.Println("Hello World")
+	setupRoutes()
+}
